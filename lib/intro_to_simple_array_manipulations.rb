89821da26967b0721d01f8d2f0792ce365e02518
Payton Gray <paytongray18@gmail.com>
2020-07-28 16:20:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat(my_favorite_things,more_favs)
   my_favorite_things = ("raindrops on roses", "whiskers on kittens")
   more_favs = ("sports cars", "flatiron school")
-  using_concat.concat(my_favorite_things,more_favs)
+  my_favorite_things.concat(more_favs)
 end
