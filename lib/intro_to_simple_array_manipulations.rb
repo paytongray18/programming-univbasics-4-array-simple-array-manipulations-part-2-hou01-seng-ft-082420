d64f2d96daa5d5cbc6347b04e06ad63630252c1e
Payton Gray <paytongray18@gmail.com>
2020-07-28 16:48:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,5 @@ def using_concat(my_favorite_things,more_favs)
 end
 
 def using_insert(list_of_programming_languages)
-  list_of_programming_languages.insert(4,"Python")
+  list_of_programming_languages.insert("Python")
 end
